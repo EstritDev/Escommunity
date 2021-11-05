a2308c1d2d8ffd64d10c6b3019a5e266644fa884
Ricardo <ricardo.jorge.silva33@gmail.com>
2021-11-05 19:11:35 +0000

    melhorias no layout(layout responsivo)

--- app/src/main/java/com/example/escommunity/PerfilActivity.java
+++ app/src/main/java/com/example/escommunity/PerfilActivity.java
@@ -2,6 +2,7 @@ package com.example.escommunity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 import android.view.View;
@@ -33,8 +34,27 @@ public class PerfilActivity extends AppCompatActivity {
 
         //Buttons
         ImageButton btnEditarPerfil = findViewById(R.id.btnEditarPerfil);
+        ConstraintLayout btnHome = findViewById(R.id.btnHome);
+        ConstraintLayout btnPerfil = findViewById(R.id.btnPerfil);
+        ConstraintLayout btnMessagens = findViewById(R.id.btnMessagens);
 
+        //Coloca o nome de utilizador por baixo da foto de perfil
         lblNome.setText(user);
+
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        btnMessagens.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(getApplicationContext(),"Ação indisponível", Toast.LENGTH_LONG).show();
+            }
+        });
 
         btnEditarPerfil.setOnClickListener(new View.OnClickListener() {
             @Override
